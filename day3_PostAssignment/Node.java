2dd36ba3ad7f32a0bf6c7eb4e633a873c4e68807
prakhalya <prakhalyasrinivasan@gmail.com>
2025-06-11 09:51:34 +0530

    Node.java

--- day3_PostAssignment/Node.java
+++ day3_PostAssignment/Node.java
@@ -1,25 +1,16 @@
 package addon;
 
-
 	import java.util.Scanner;
-
 	public class Node {
-	
 	    int data;
-	    Node next;
-
-	   
+	    Node next;   
 	    Node(int data) {
 	        this.data = data;
 	        this.next = null;
 	    }
-	}
-
-	
+	}	
 	class Link {
-	    Node head;
-
-	   
+	    Node head;   
 	    public void insert(int data) {
 	        Node newNode = new Node(data);
 
@@ -27,31 +18,24 @@ package addon;
 	            head = newNode;
 	        } else {
 	            Node temp = head;
-
 	            while (temp.next != null) {
 	                temp = temp.next;
 	            }
-
 	            temp.next = newNode;
 	        }
-
-	        System.out.println("Inserted: " + data);
-	        
+	        System.out.println("Inserted: " + data);        
 	    }
 	    public void delete(int position) {
 	        if (head == null) {
 	            System.out.println("List is empty.");
 	            return;
 	        }
-
 	        if (position == 0) {
 	            System.out.println("Deleted: " + head.data);
 	            head = head.next;
 	            return;
 	        }
-
 	        Node temp = head;
-
 	        for (int i = 0; i < position - 1; i++) {
 	            if (temp == null || temp.next == null) {
 	                System.out.println("Position out of bounds.");
@@ -59,12 +43,10 @@ package addon;
 	            }
 	            temp = temp.next;
 	        }
-
 	        if (temp.next == null) {
 	            System.out.println("Position out of bounds.");
 	            return;
 	        }
-
 	        System.out.println("Deleted: " + temp.next.data);
 	        temp.next = temp.next.next;
 	    }
@@ -73,7 +55,6 @@ package addon;
 	            System.out.println("List is empty.");
 	            return;
 	        }
-
 	        Node temp = head;
 	        System.out.print("Linked List: ");
 	        while (temp != null) {
@@ -86,7 +67,6 @@ package addon;
 	        Scanner sc = new Scanner(System.in);
 	        Link list = new Link();
 	        int choice;
-
 	        do {
 	            System.out.println("\n--- Singly Linked List Menu ---");
 	            System.out.println("1. Insert at End");
@@ -94,36 +74,28 @@ package addon;
 	            System.out.println("3. Traverse");
 	            System.out.println("4. Exit");
 	            System.out.print("Enter your choice: ");
-
 	            choice = sc.nextInt();
-
 	            switch (choice) {
 	                case 1:
 	                    System.out.print("Enter value to insert: ");
 	                    int val = sc.nextInt();
 	                    list.insert(val);
 	                    break;
-
 	                case 2:
 	                    System.out.print("Enter position to delete (0-based): ");
 	                    int pos = sc.nextInt();
 	                    list.delete(pos);
 	                    break;
-
 	                case 3:
 	                    list.traverse();
 	                    break;
-
 	                case 4:
 	                    System.out.println("Exiting program.");
 	                    break;
-
 	                default:
 	                    System.out.println("Invalid choice. Please try again.");
 	            }
-
 	        } while (choice != 4);
-
 	        sc.close();
 	    }
 	}
